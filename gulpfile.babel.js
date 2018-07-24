37b6df55db5e2b6319fe4b0c14e7e2c59635cd76
Martin Wright <martin@infuze.co.uk>
2018-07-24 17:48:25 +0100

    Update gulp: build multiple packs from build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,9 +15,124 @@ import replace from 'gulp-replace';
 import "babel-polyfill";
 import autoprefixer from 'gulp-autoprefixer';
 
+const newer = require('gulp-newer');
+import fs from 'fs';
+
+const merge = require('merge-stream');
+
+
+let packs = [];
+for (let i = 1; i <= 32; i++) {
+    packs.push(i);
+}
 /* ----------------- */
 /* Development
 /* ----------------- */
+
+
+gulp.task('unit-folders', () => {
+    let folders = [];
+    for (let i = 1; i <= 32; i++) {
+        folders.push('packs/unit-' + i + '/');
+    }
+    if (!fs.existsSync('packs'))
+        fs.mkdirSync('packs'),
+            console.log('📁  folder created:', 'packs');
+
+    folders.forEach(dir => {
+        if (!fs.existsSync(dir))
+            fs.mkdirSync(dir),
+                console.log('📁  folder created:', dir);
+    });
+
+});
+
+/* ----------------- */
+/* Pack Libs
+/* ----------------- */
+gulp.task('pack-libs', () => {
+    // Copy folders
+    for (let i = 1; i <= 32; i++) {
+        gulp.src('build/libs/**')
+            .pipe(plumber({
+                errorHandler: onError
+            }))
+            .pipe(gulp.dest(`packs/unit-${i}/libs`))
+            .pipe(notify({message: `Libs copy unit-${i}/libs complete`}))
+        /*.pipe(gulp.src('build/js/!**'))
+        .pipe(gulp.dest(`packs/unit-${i}/js`))
+        .pipe(notify({message: `Libs copy unit-${i}/js complete`}));*/
+    }
+});
+/* ----------------- */
+/* Pack JS
+/* ----------------- */
+gulp.task('pack-js', () => {
+
+        let copyCSS = packs.map(pack => {
+            let cssSrc = `build/css/**`,
+                cssDest = `packs/unit-${pack}/css`;
+            return gulp.src(cssSrc)
+                .pipe(newer(cssDest))
+                .pipe(plumber({errorHandler: onError}))
+                .pipe(gulp.dest(cssDest))
+                .pipe(notify({message: `js copy ${pack} task complete`}))
+        });
+
+        let copyJS = packs.map(pack => {
+            let jsSrc = `build/js/**`,
+                jsDest = `packs/unit-${pack}/js`;
+            return gulp.src(jsSrc)
+                .pipe(newer(jsDest))
+                .pipe(plumber({errorHandler: onError}))
+                .pipe(gulp.dest(jsDest))
+                .pipe(notify({message: `js copy ${pack} task complete`}))
+        });
+        let copyLibs = packs.map(pack => {
+            let libSrc = `build/libs/**`,
+                libDest = `packs/unit-${pack}/libs`;
+            return gulp.src(libSrc)
+                .pipe(newer(libDest))
+                .pipe(plumber({errorHandler: onError}))
+                .pipe(gulp.dest(libDest))
+                .pipe(notify({message: `libs copy ${pack} task complete`}))
+        });
+        let copyImgs = packs.map(pack => {
+            let imgSrc = `build/images/u${pack}/**`,
+                imgDest = `packs/unit-${pack}/images/u${pack}`;
+            return gulp.src(imgSrc)
+                .pipe(newer(imgDest))
+                .pipe(plumber({errorHandler: onError}))
+                .pipe(imagemin())
+                .pipe(gulp.dest(imgDest))
+                .pipe(notify({message: `imgs copy ${pack} task complete`}))
+        });
+        return merge(copyJS, copyLibs, copyImgs, copyCSS);
+
+        /*for (let key in packs) {
+            (function (key) {
+                /!*gulp.task(key, function() {
+                    return gulp.src(jsFiles[key])
+                        .pipe(jshint())
+                        .pipe(uglify())
+                        .pipe(concat(key + '.js'))  // <- HERE
+                        .pipe(gulp.dest('public/js'));
+                });*!/
+
+                gulp.src('build/js/!**')
+                    .pipe(plumber({
+                        errorHandler: onError
+                    }))
+                    .pipe(gulp.dest(`packs/unit-${key}/js`))
+                    .pipe(notify({message: `JS copy ${key} task complete`}));
+
+            })(key);
+        }*/
+
+
+    }
+);
+
 
 gulp.task('development', ['scripts', 'styles', 'images', 'html'], () => {
     browserSync({
@@ -41,11 +156,10 @@ gulp.task('development', ['scripts', 'styles', 'images', 'html'], () => {
 
 
 // Copy dependencies to ./build/libs/
-gulp.task('libs', function() {
-    gulp.src(npmDist(), {base:'./node_modules'})
+gulp.task('libs', function () {
+    gulp.src(npmDist(), {base: './node_modules'})
         .pipe(gulp.dest('./build/libs'));
 });
-
 
 
 /* ----------------- */
@@ -112,11 +226,9 @@ gulp.task('scripts', () => {
 
 
 // Gulp plumber error handler
-var onError = function(err) {
+var onError = function (err) {
     console.log(err);
 }
-
-
 
 
 /* ----------------- */
@@ -220,3 +332,4 @@ gulp.task('jsmin', () => {
 
 gulp.task('default', ['development']);
 gulp.task('deploy', ['html', 'images', 'jsmin', 'libs', 'vendor', 'fonts']);
+gulp.task('pack', ['unit-folders', 'pack-js']);
